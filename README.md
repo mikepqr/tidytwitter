b25c4dd1536d7ecfe7b72445a2cefc90bb3ed8ef
Mike Lee Williams <mike@mike.place>
2020-10-15 21:05:09 -0700

    Log no-ops at INFO, try to read environment first

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # twittertidy
 
-Delete your old tweets and favorites using the Twitter API
+Delete your old tweets and favorites using the Twitter API.
+
+Based on https://gist.github.com/chrisalbon/b9bd4a6309c9f5f5eeab41377f27a670.
--- src/twittertidy/twittertidy.py
+++ src/twittertidy/twittertidy.py
@@ -1,61 +1,78 @@
-# Based on https://gist.github.com/chrisalbon/b9bd4a6309c9f5f5eeab41377f27a670
-
 import json
 import logging
+import os
 from datetime import datetime
 
 import tweepy
 
-with open("auth.json") as f:
-    auth_data = json.load(f)
-
-auth = tweepy.OAuthHandler(auth_data["api_key"], auth_data["api_secret_key"])
-auth.set_access_token(auth_data["access_token"], auth_data["access_token_secret"])
-
-api = tweepy.API(
-    auth,
-    wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True,
-    retry_count=3,
-    retry_delay=5,
-    retry_errors=set([401, 404, 500, 503]),
-)
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
-def deletetweets(favorite_threshold=20, days=62, dry_run=True):
+def create_api():
+    if (
+        "TWITTER_API_KEY" in os.environ
+        and "TWITTER_API_SECRET_KEY" in os.environ
+        and "TWITTER_ACCESS_TOKEN" in os.environ
+        and "TWITTER_ACCESS_TOKEN_SECRET" in os.environ
+    ):
+        auth_data = {}
+        auth_data["api_key"] = os.environ["TWITTER_API_KEY"]
+        auth_data["api_secret_key"] = os.environ["TWITTER_API_SECRET_KEY"]
+        auth_data["access_token"] = os.environ["TWITTER_ACCESS_TOKEN"]
+        auth_data["access_token_secret"] = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+    elif os.path.isfile("auth.json"):
+        with open("auth.json") as f:
+            auth_data = json.load(f)
+    else:
+        raise OSError("Environment variables not present and auth.json not found")
+
+    auth = tweepy.OAuthHandler(auth_data["api_key"], auth_data["api_secret_key"])
+    auth.set_access_token(auth_data["access_token"], auth_data["access_token_secret"])
+
+    return tweepy.API(
+        auth,
+        wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True,
+        retry_count=3,
+        retry_delay=5,
+        retry_errors=set([401, 404, 500, 503]),
+    )
+
+
+def delete_tweets(api, favorite_threshold=20, days=62, dry_run=True):
     for status in tweepy.Cursor(api.user_timeline).items():
-        logging.info(f"Examining tweet {status.id}")
+        logging.debug(f"Examining tweet {status.id}")
 
         if (datetime.utcnow() - status.created_at).days <= days:
-            logging.warning(f"Skipping tweet (recent) {status.id}")
+            logging.info(f"Skipping tweet (recent) {status.id}")
             continue
         if status.favorite_count > favorite_threshold:
-            logging.warning(f"Skipping tweet (lots of favorites) {status.id}")
+            logging.info(f"Skipping tweet (lots of favorites) {status.id}")
             continue
         if status.favorited:
-            logging.warning(f"Skipping tweet (self-favorited) {status.id}")
+            logging.info(f"Skipping tweet (self-favorited) {status.id}")
             continue
         if dry_run:
-            logging.warning(f"Skipping (dry run) {status.id}")
+            logging.info(f"Skipping (dry run) {status.id}")
             continue
 
         logging.warning(f"Deleting tweet {status.id}")
         api.destroy_status(status.id)
 
 
-def deletefavorites(days=62, dry_run=True):
+def delete_favorites(api, days=62, dry_run=True):
     me = api.me().id
     for status in tweepy.Cursor(api.favorites).items():
-        logging.info(f"Examining {status.id}")
+        logging.debug(f"Examining {status.id}")
 
         if (datetime.utcnow() - status.created_at).days <= days:
-            logging.warning(f"Skipping favorite (recent) {status.id}")
+            logging.info(f"Skipping favorite (recent) {status.id}")
             continue
         if status.user.id == me:
-            logging.warning(f"Skipping favorite (self-favorited) {status.id}")
+            logging.info(f"Skipping favorite (self-favorited) {status.id}")
             continue
         if dry_run:
-            logging.warning(f"Skipping favorite (dry run) {status.id}")
+            logging.info(f"Skipping favorite (dry run) {status.id}")
             continue
 
         logging.warning(f"Deleting favorite {status.id}")
@@ -63,9 +80,7 @@ def deletefavorites(days=62, dry_run=True):
 
 
 def main():
-    deletetweets(dry_run=True)
-    deletefavorites(dry_run=True)
-
-
-if __name__ == "__main__":
-    main()
+    api = create_api()
+    logging.info(f"Deleting tweets and favorites for @{api.me().screen_name}")
+    delete_tweets(api, dry_run=True)
+    delete_favorites(api, dry_run=True)
